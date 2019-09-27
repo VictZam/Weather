3e4a34d73d9f0c6424af105d1166290c8873e0c5
unknown <victor.zamora1583@gmail.com>
2019-09-27 13:16:15 -0600

    Se genera el fragmento que mostrara la lista e informacion del pais seleccionado

---
+++ app/src/main/java/com/weather/data/db/CityList.java
@@ -0,0 +1,33 @@
+package com.weather.data.db;
+
+public class CityList {
+
+    private int cityId;
+    private String name;
+    private String country;
+
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
